63b09f53372c3e6450a76ceabc1b0d832b1e5a95
jeffery.raimundo@medaire.com <jeff_pepper23@yahoo.com>
2019-05-19 15:04:52 -0700

    Folder Structure setup (per homework instructions)

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS buregers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+	id INT (10) AUTO_INCREMENT NOT NULL,
+    burger_Name VARCHAR(255) NOT NULL,
+    devoured BOOLEAN DEFAULT 0,
+    PRIMARY KEY (id)
+);
+
+SELECT * FROM burgers;
---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+USE burgers_db;
+
+insert into burgers (burger_name)
+
+	VALUES ("Cheese burger"), ("Bacon burger"), ("Chili burger");
+
+
+
+select * from burgers;
