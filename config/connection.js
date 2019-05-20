1451fe7265adc41a4b64de8ff3b402719a2af20c
jeffery.raimundo@medaire.com <jeff_pepper23@yahoo.com>
2019-05-19 17:28:38 -0700

    more html. Connections created

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+
+const mysql = require("mysql");
+
+let connection = mysql.createConnection({
+
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "Redbird7821037*",
+        database: "burgers_db"
+
+    });
+
+connection.connect((error) {
+
+    if(error) {
+        console.error("Error connecting: " + err.stack);
+        return;
+   };
+
+    console.log("Connected as id " + connection.threadId);
+
+});
+
+module.exports = connection;
